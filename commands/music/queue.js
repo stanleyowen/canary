a2546a286afeab06c7b562b9674e807cc71d2033
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2023-05-03 18:49:17 +0700

    fix: fixed issue of app error when playing `/queue` command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -4,27 +4,24 @@ const { useQueue } = require("discord-player");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("queue")
-    .setDescription("🎧 Shows the queue of the current playing song.")
+    .setDescription("🎶 Shows the queue of the current playing song.")
     .setDMPermission(false),
 
   async execute(interaction) {
-    if (!interaction.member?.voice?.channel) {
+    if (!interaction.member?.voice?.channel)
       return await interaction.reply({
-        content: "You are not in a voice channel!",
-        ephemeral: true,
+        content: "❌ You are not in a voice channel!",
       });
-    }
 
     if (
       interaction.guild.members.me.voice.channelId &&
       interaction.member.voice.channelId !==
         interaction.guild.members.me.voice.channelId
-    ) {
+    )
       return await interaction.reply({
-        content: "You are not in my voice channel!",
+        content: "❌ You are not in the same voice channel as Canary.",
         ephemeral: true,
       });
-    }
 
     const queue = useQueue(interaction.guildId);
 
@@ -33,26 +30,30 @@ module.exports = {
       const { currentTrack } = queue;
 
       const embed = new EmbedBuilder()
+        .setColor("#87CEEB")
         .setTitle("Queue")
         .setDescription(
-          tracks
-            .map(
-              (track, index) =>
-                `${index + 1}. **${track.title}** - ${track.duration}`
-            )
-            .join("\n")
+          tracks && tracks.length
+            ? tracks
+                .map(
+                  (track, index) =>
+                    `${index + 1}. **${track.title}** - ${track.duration}`
+                )
+                .join("\n")
+            : null
         )
         .setFooter({
-          text: `Now playing: ${currentTrack.title} - ${currentTrack.duration}`,
-        })
-        .setColor("#87CEEB");
+          text: `🎶 Now playing: ${currentTrack.title} - ${currentTrack.duration}`,
+        });
       await interaction.reply({ embeds: [embed] });
     } else {
       await interaction.reply({
         embeds: [
           new EmbedBuilder()
-            .setTitle("Queue")
-            .setDescription("There are no songs in the queue.")
+            .setTitle("🎶 Queue List")
+            .setDescription(
+              "❌ There are currently no songs in the queue.\nTo add a song, use `/play`."
+            )
             .setColor("#87CEEB"),
         ],
       });
